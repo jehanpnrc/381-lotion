18fa7e02b2d41bd830e61361c75b06ff7716087c
jehanpnrc <jehan.pinaroc@ucalgary.ca>
2023-03-07 19:40:05 -0700

    delete function works

--- src/Preview.js
+++ src/Preview.js
@@ -1,13 +1,13 @@
 import { useOutletContext } from "react-router-dom";
 
 function Preview() {
-    const [activeNote, onUpdateNote] = useOutletContext();
+    const [activeNote, onUpdateNote, onDeleteNote] = useOutletContext();
     return (
         <div className='main_container'>
             <div className='title_area'>
                 <h2 className="title_pp">{activeNote.title}</h2>
                 <button className ='note_button'>Edit</button>
-                <button className='note_button'>Delete</button>
+                <button className="note_button" onClick={() => onDeleteNote(activeNote.id)}>Delete</button>
             </div>
                 <small>{new Date(activeNote.lastModified).toLocaleDateString("eg-GB", {
                   year: "numeric",
